cd10ca18ddec654cab0a1b993a2a1736d3d87db1
Natanael Lima <taelima1997@gmail.com>
2020-02-12 16:56:43 -0300

    create global style

---
+++ src/styles/global.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
+*{
+  margin : 0;
+  padding : 0;
+  outline : 0;
+  box-sizing : border-box;
+}
+
+*:focus{
+  outline : 0;
+}
+
+html,body,#root{
+  height : 100%;
+}
+body{
+  -webkit-font-smoothing: antialiased !important;
+}
+body,input,button{
+   font-size : 14px;
+  font-family: 'Roboto', sans-serif;
+}
+
+a{
+  text-decoration : none;
+}
+
+ul{
+  list-style : none;
+}
+
+button{
+  cursor: pointer;
+}
+`;
