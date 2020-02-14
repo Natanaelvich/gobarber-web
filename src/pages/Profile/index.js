bc6670d83b7f7975996876ee32e7464e88568057
Natanael Lima <taelima1997@gmail.com>
2020-02-14 19:01:46 -0300

    created user page and your styles

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import * as Yup from 'yup';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Container } from './Styles';
+
+const schema = Yup.object().shape({
+  name: Yup.string(),
+  email: Yup.string().email('Insira um email valido'),
+  password: Yup.string(),
+  oldPassword: Yup.string(),
+  cofirmPassword: Yup.string(),
+});
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <Container>
+      <Form initialData={profile} schema={schema}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input
+          name="email"
+          placeholder="Seu endereço de e-mail"
+          type="email"
+        />
+        <hr />
+        <Input
+          name="password"
+          placeholder="Sua senha antiga"
+          type="password"
+        />
+        <Input
+          name="oldPassword"
+          placeholder="Sua nova senha"
+          type="password"
+        />
+        <Input
+          name="CofirmPassword"
+          placeholder="Confime sua nova senha"
+          type="password"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="submit">Sair do Gobarber</button>
+    </Container>
   );
 }
