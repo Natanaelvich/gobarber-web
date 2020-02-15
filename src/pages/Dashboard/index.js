ee22cde6548d0ac7567eef47a0b9c927162ec5a6
Natanael Lima <taelima1997@gmail.com>
2020-02-15 03:16:30 -0300

    formated date dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-
+import React, { useState, useMemo } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormated = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
   return (
     <Container>
       <header>
-        <button type="button">
+        <button onClick={handlePrevDay} type="button">
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>31 de Maio</strong>
-        <button type="button">
+        <strong>{dateFormated}</strong>
+        <button onClick={handleNextDay} type="button">
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
